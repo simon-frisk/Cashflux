fe4d47f21934beffd4971ad5271881f2b79016d7
Simon Frisk <simon@simonfrisk.com>
2020-11-22 17:12:59 +0100

    Fix date picker and update icon

--- src/AddExpense.js
+++ src/AddExpense.js
@@ -1,16 +1,17 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { FlatList, TouchableOpacity, View } from 'react-native'
 import SButton from './components/SButton'
 import STextField from './components/STextField'
 import SText from './components/SText'
-import { Entypo } from '@expo/vector-icons'; 
+import { Entypo } from '@expo/vector-icons'
+import DateTimePicker from '@react-native-community/datetimepicker'
 
 
 export default ({categories, addExpense}) => {
   const [isToggled, toggle] = useState(false)
 
   const [category, setCategory] = useState(1)
-  const [date, setDate] = useState(Date.now())
+  const [date, setDate] = useState(new Date())
   const [text, setText] = useState('')
   const [cost, setCost] = useState('')
 
@@ -27,12 +28,12 @@ export default ({categories, addExpense}) => {
             <STextField placeholder='Cost' style={{width: 100, marginRight: 5}} value={cost} onChangeText={setCost} />
             <CategoryPicker categories={categories} category={category} setCategory={setCategory} />
           </View>
-          <DatePicker date={date} setDate={setDate} />
+          <DatePicker date={date} onDateChange={setDate} />
           <SButton text='Add Expense' action={() => {
-            addExpense({date: new Date(date).toDateString(), text, cost: Number(cost), category})
+            addExpense({date: date.toDateString(), text, cost: Number(cost), category})
             setText('')
             setCost('')
-            setDate(Date.now())
+            setDate(new Date())
             setCategory(1)
           }} />
       </View>
@@ -45,32 +46,31 @@ function CategoryPicker({categories, category, setCategory}) {
   return (
     <FlatList data={categories}  horizontal={true} renderItem={({item}) => (
       <TouchableOpacity style={{backgroundColor: item.id == category ? '#f80' : '#777', marginHorizontal: 3, padding: 5, borderRadius: 5, marginVertical: 5}} onPress={() => setCategory(item.id)}>
-        <SText >{item.name}</SText>
+        <SText>{item.name}</SText>
       </TouchableOpacity>
     )} />
   )
 }
 
-function DatePicker({date, setDate}) {
-  const [year, setYear] = useState(new Date(date).getFullYear().toString())
-  const [month, setMonth] = useState(new Date(date).getMonth().toString())
-  const [day, setDay] = useState(new Date(date).getDate().toString())
-
-  useEffect(() => {
-    if(year && month && day) {
-      date = new Date()
-      date.setYear(year)
-      date.setMonth(month)
-      date.setDate(date)
-      setDate(date)
-    }
-  }, [day, month, year])
+function DatePicker({date, onDateChange}) {
+  const [show, setShow] = useState(false)
 
   return (
-    <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-      <STextField style={{width: '45%'}} maxLength={4} value={year} onChangeText={setYear} />
-      <STextField style={{width: '25%'}} maxLength={2} value={month} onChangeText={setMonth} />
-      <STextField style={{width: '25%'}} maxLength={2} value={day} onChangeText={setDay} />
+    <View>
+      <SText style={{textAlign: 'center'}}>{date.toDateString()}</SText>
+      {show && (
+        <DateTimePicker
+          value={date}
+          mode='date'
+          display='default'
+          onChange={(_, selectedDate) => {
+            const currentDate = selectedDate || date
+            setShow(Platform.OS === 'ios')
+            onDateChange(currentDate)
+          }}
+        />
+      )}
+      <SButton text='Change date' action={() => setShow(!show)} />
     </View>
   )
 }
