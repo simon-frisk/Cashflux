f9b919f58a9f88a0aa055c5e2eef755875f3eae8
Simon Frisk <simon@simonfrisk.com>
2020-11-22 17:18:38 +0100

    Fix expense text

--- src/Expense.js
+++ src/Expense.js
@@ -5,12 +5,12 @@ import { AntDesign } from '@expo/vector-icons';
 
 export default ({expense, deleteExpense}) => {
   return (
-    <View style={{paddingVertical: 8, borderTopColor: '#bbb', borderTopWidth: 1, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
-      <View>
+    <View style={{paddingVertical: 8, borderTopColor: '#bbb', borderTopWidth: 1}}>
+      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
         <SText fontSize={23}>{expense.text}</SText>
-        <SText color='#bbb'>{expense.category.emoji} {expense.category.name} - {expense.date} - {expense.cost}kr</SText>
+        <TouchableOpacity onPress={() => deleteExpense(expense.id)}><AntDesign name="delete" size={24} color="#d55" /></TouchableOpacity>
       </View>
-      <TouchableOpacity onPress={() => deleteExpense(expense.id)}><AntDesign name="delete" size={24} color="#d55" /></TouchableOpacity>
+      <SText color='#bbb'>{expense.category.emoji} {expense.category.name} - {expense.date} - {expense.cost}kr</SText>
     </View>
   )
 }
