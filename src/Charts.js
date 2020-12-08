8867eeb9f088e9a113b08968d25d33b05a6f8cb5
Simon Frisk <simon@simonfrisk.com>
2020-12-08 19:57:59 +0100

    Add text and month filter to expense pie

--- src/Charts.js
+++ src/Charts.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { FlatList, View, Dimensions } from 'react-native'
 import SText from './components/SText'
 import { PieChart, StackedBarChart } from 'react-native-svg-charts'
+import { get } from 'react-native/Libraries/Utilities/PixelRatio'
 
 export default function Charts({categories, expenses}) {
 
@@ -26,9 +27,12 @@ export default function Charts({categories, expenses}) {
 }
 
 function ExpensePie({categories, expenses}) {
+    const getTime = date => date.split(' ')[1] + date.split(' ')[3]
+    const time = getTime(new Date().toDateString())
+
     const pieData = categories
         .map(category => ({
-            value: expenses.filter(expense => expense.category.id == category.id).reduce((total, expense) => total + expense.cost, 0),
+            value: expenses.filter(expense => (expense.category.id === category.id && getTime(expense.date) === time)).reduce((total, expense) => total + expense.cost, 0),
             svg: {
                 fill: category.color,
             },
@@ -36,12 +40,17 @@ function ExpensePie({categories, expenses}) {
             arc: { cornerRadius: 7,  }
             }))
     
-    const total = expenses.reduce((total, expense) => total + expense.cost, 0)
+    const total = expenses
+                    .filter(expense => getTime(expense.date) === time)
+                    .reduce((total, expense) => total + expense.cost, 0)
 
     return <View>
-                <PieChart style={{ height: 200}} innerRadius='70%' labelRadius='10px' padAngle={.04} data={pieData} />
-                <View style={{position: 'absolute', height: '100%', width: '100%', justifyContent: 'center', alignItems: 'center'}}>
-                    <SText fontSize={35}>{total}</SText>
+                <SText fontSize={22}>This month</SText>
+                <View>
+                    <PieChart style={{ height: 200, marginTop: 10}} innerRadius='70%' labelRadius='10px' padAngle={.04} data={pieData} />
+                    <View style={{position: 'absolute', height: '100%', width: '100%', justifyContent: 'center', alignItems: 'center'}}>
+                        <SText fontSize={35}>{total}</SText>
+                    </View>
                 </View>
             </View>
 }
