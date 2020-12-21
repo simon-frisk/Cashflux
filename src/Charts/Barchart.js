fbc69f5de275a3aad84d0d46b49114fe24d6216b
Simon Frisk <simon@simonfrisk.com>
2020-12-21 19:25:19 +0100

    Add barchart select and enlarge feature

--- src/Charts/Barchart.js
+++ src/Charts/Barchart.js
@@ -4,9 +4,12 @@ import SText from '../components/SText'
 import dataContext from '../dataContext'
 import { getMonthlyCategories } from '../util/DateTools'
 import { getCostString } from '../util/currency'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 
 export default function BarChart({width, active}) {
   const {expenses, categories, currency} = useContext(dataContext)
+
+  const [selectedMonth, setSelectedMonth] = useState(null)
 
   const monthlyCategories = getMonthlyCategories(expenses)
   const highestMonthCost = Math.max(...monthlyCategories.map(category => category.total))
@@ -17,14 +20,18 @@ export default function BarChart({width, active}) {
         showsHorizontalScrollIndicator={false}
         bounces={false}
         style={{width}}
+        contentContainerStyle={{paddingBottom: 20}}
       >
-        {monthlyCategories.map(month => (
+        {monthlyCategories.map((month, index) => (
           <MonthBar
             currency={currency}
+            selected={index == selectedMonth}
+            toggleSelected={() => setSelectedMonth(index == selectedMonth ? null : index)}
             month={month}
             categories={categories}
             highestMonthCost={highestMonthCost}
             active={active}
+            isTransparent={selectedMonth !== null && selectedMonth !== index}
             key={month.string}
           />
         ))}
@@ -32,34 +39,69 @@ export default function BarChart({width, active}) {
   )
 }
 
-function MonthBar({currency, month, categories, highestMonthCost, active}) {
+function MonthBar({currency, month, categories, highestMonthCost, active, selected, toggleSelected, isTransparent}) {
+  const [zoomAnimation] = useState(new Animated.Value(1))
+  const [opacityAnimation] = useState(new Animated.Value(1))
+
+  useEffect(() => {
+    Animated.timing(zoomAnimation, {
+      toValue: selected ? 1.07 : 1,
+      duration: 1000,
+      useNativeDriver: true
+    }).start()
+  }, [selected])
+
+  useEffect(() => {
+    Animated.timing(opacityAnimation, {
+      toValue: isTransparent ? 0.4 : 1,
+      duration: 1000,
+      useNativeDriver: true
+    }).start()
+  }, [isTransparent])
+
   return (
-    <View
+    <Animated.View
       style={{
         height: '100%',
         flexDirection: 'row',
         alignItems: 'flex-end',
-        marginHorizontal: 5,
+        marginHorizontal: 7,
+        marginBottom: 15,
+        opacity: opacityAnimation,
+        transform: [
+          {
+            scale: zoomAnimation
+          }
+        ]
       }}
     >
-      <View>
+      <TouchableOpacity onPress={() => toggleSelected()}>
         <SText fontSize={15}>{getCostString(month.total, currency)}</SText>
         {categories.map(category => {
-          const current = month[category.id] || 0
-          const height = current / highestMonthCost * 300
-            if(height < 5) return
-            else return (
-              <BarBlock height={height} category={category} key={category.name.toString() + month.string} active={active} />
+            return (
+              <BarBlock 
+                isSelected={selected} 
+                month={month} 
+                highestMonthCost={highestMonthCost} 
+                category={category} 
+                key={category.name.toString() + month.string} 
+                active={active}
+              />
             )
         })}
         <SText fontSize={25}>{month.string}</SText>
-      </View>
-    </View>
+      </TouchableOpacity>
+    </Animated.View>
   )
 }
 
-function BarBlock({height, category, active}) {
+function BarBlock({isSelected, month, highestMonthCost, category, active}) {
   const [heightAnimation] = useState(new Animated.Value(0))
+  const [height, setHeight] = useState(getHeight())
+
+  useEffect(() => {
+    setHeight(getHeight())
+  }, [isSelected, month, highestMonthCost, category])
 
   useEffect(() => {
     Animated.timing(heightAnimation, {
@@ -67,13 +109,23 @@ function BarBlock({height, category, active}) {
       duration: 1000,
       useNativeDriver: false
     }).start()
-  }, [active])
+  }, [active, height])
 
+  function getHeight() {
+    const current = month[category.id] || 0
+    const height = isSelected
+      ? current / month.total * 280
+      : current / highestMonthCost * 280
+    return height
+  }
+
+  if(height < 5) return <View />
+    
   return (
     <Animated.View
       style={{
         height: heightAnimation,
-        width: 60,
+        width: 55,
         backgroundColor: category.color, 
         marginVertical: 2, 
         borderRadius: 5,
