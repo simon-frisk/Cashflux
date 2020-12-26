4bf38c7dad7017a446609c0746e9107463401f78
Simon Frisk <simon@simonfrisk.com>
2020-12-26 11:13:46 +0100

    Clean up

--- src/components/SPageContainer.js
+++ src/components/SPageContainer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ScrollView, View } from "react-native"
+import { ScrollView } from "react-native"
 
 export default function SPageContainer(props) {
   //Container for whole page
