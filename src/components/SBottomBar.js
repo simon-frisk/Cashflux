d0c2774aed58371200415a216ffbf6396a8a7681
Simon Frisk <simon@simonfrisk.com>
2020-12-27 17:53:40 +0100

    Add safeareaview to SBottomBar

--- src/components/SBottomBar.js
+++ src/components/SBottomBar.js
@@ -1,29 +1,35 @@
 import React from 'react'
-import { View } from 'react-native'
+import { SafeAreaView, View } from 'react-native'
 import useStyle from '../util/useStyle'
 
 export default function SBottomBar({children}) {
   const style = useStyle()
 
   return (
-    <View
+    <SafeAreaView 
       style={{
+        flex: 1, 
         position: 'absolute',
         bottom: 0,
         left: 0,
         width: '100%',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        paddingBottom: 20,
-        paddingHorizontal: 30,
-        paddingTop: 10,
         backgroundColor: style.backgroundColor,
         borderTopWidth: .3,
-        borderTopColor: style.lightText
+        borderTopColor: style.lightText,
+        
       }}
     >
-      {children}
-    </View>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          paddingHorizontal: 30,
+          paddingVertical: 10,
+        }}
+      >
+        {children}
+      </View>
+    </SafeAreaView>
   )
 }
