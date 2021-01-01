240c6a314b1d58cb959e05a32a39289772bee3bb
Simon Frisk <simon@simonfrisk.com>
2021-01-01 15:45:48 +0100

    Update expense list selector

--- src/Home/ExpenseList.js
+++ src/Home/ExpenseList.js
@@ -1,36 +1,81 @@
 import React, { useContext, useState } from 'react'
-import { View } from 'react-native'
+import { FlatList, TouchableOpacity, View } from 'react-native'
 import SText from '../components/SText'
+import { useNavigation } from '@react-navigation/native'
 import dataContext from '../dataContext'
-import { getMonthlyExpenses} from '../util/DateTools'
+import { getMonthlyExpenses, getDayString } from '../util/DateTools'
 import useStyle from '../util/useStyle'
-import SSelectionSlider from '../components/SSelectionSlider'
-import Expense from './Expense'
+import { getCostString } from '../util/currency'
 
 export default () => {
   const {expenses} = useContext(dataContext)
+  const style = useStyle()
 
   const monthlyExpenses = getMonthlyExpenses(expenses)
-
   const defaultMonth = monthlyExpenses.length != 0 ? monthlyExpenses[0].string : null
   const [selectedMonth, setSelectedMonth] = useState(defaultMonth)
-
   const monthExpenses = monthlyExpenses.find(month => month.string == selectedMonth)
   
   return (
     <View
       style={{marginTop: 15, marginBottom: 30}}
     >
-      <SSelectionSlider
-        items={monthlyExpenses.map(month => month.string)}
-        selected={selectedMonth}
-        setSelected={setSelectedMonth}
+      <FlatList
+        data={monthlyExpenses.map(month => month.string)}
+        horizontal={true}
         keyExtractor={month => month}
-        textExtractor={month => month}
+        contentContainerStyle={{paddingBottom: 7, alignItems: 'center'}}
+        showsHorizontalScrollIndicator={false}
+        renderItem={({item: month}) => (
+          <TouchableOpacity
+            onPress={() => setSelectedMonth(month)}
+            style={{
+              borderBottomColor: style.primaryColor,
+              borderBottomWidth: selectedMonth == month ? 4 : 0,
+              marginRight: 10,
+            }}
+          >
+            <SText
+              fontSize={selectedMonth == month ? 25 : 20}
+              color={selectedMonth == month ? style.text : style.lightText}
+            >{month}</SText>
+          </TouchableOpacity>
+        )}
       />
-      {!!monthExpenses && monthExpenses.expenses.map(expense => (
-        <Expense expense={expense} key={expense.id.toString()} />
-      ))}
+      <View
+        style={{
+          borderTopColor: style.light, 
+          borderTopWidth: .5,
+          borderBottomColor: style.light, 
+          borderBottomWidth: .5
+        }}
+      >
+        {!!monthExpenses && monthExpenses.expenses.map(expense => (
+          <Expense expense={expense} key={expense.id.toString()} />
+        ))}
+      </View>
     </View>
+  )
+}
+
+function Expense({expense}) {
+  const navigation = useNavigation()
+  const style = useStyle()
+  const {currency} = useContext(dataContext)
+
+  return (
+    <TouchableOpacity 
+      style={{
+        paddingVertical: 5, 
+        borderTopColor: style.light, 
+        borderTopWidth: .5,
+        borderBottomColor: style.light, 
+        borderBottomWidth: .5
+      }} 
+      onPress={() => navigation.navigate('Expense', {expense})}
+    >
+      <SText fontSize={25}>{expense.text}</SText>
+      <SText color={style.lightText}>{expense.category.emoji} {expense.category.name} - {getDayString(expense.date)} - {getCostString(expense.cost, currency)}</SText>
+    </TouchableOpacity>
   )
 }
